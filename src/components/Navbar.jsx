70e0b301ca56c32b7d7aa1870c4c52a9ca0c8c18
Juan Cabrera <juanm.cabrera0513@gmail.com>
2026-07-29 23:44:41 -0500

    Make home navigation scroll to top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,15 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const closeMenu = () => setIsOpen(false);
+  const goHome = () => {
+    closeMenu();
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
 
   return (
     <header className="navbar">
       <div className="container navbar__inner">
-        <Link to="/" className="navbar__logo" onClick={closeMenu}>
+        <Link to="/" className="navbar__logo" onClick={goHome}>
           <span>The Property Cousins</span>
           <small>Real Estate Team</small>
         </Link>
@@ -26,7 +30,7 @@ function Navbar() {
         </button>
 
         <nav className={`navbar__links ${isOpen ? "navbar__links--open" : ""}`}>
-          <NavLink to="/" onClick={closeMenu}>Home</NavLink>
+          <NavLink to="/" onClick={goHome}>Home</NavLink>
           <NavLink to="/about" onClick={closeMenu}>About Us</NavLink>
           <NavLink to="/listings" onClick={closeMenu}>Listings</NavLink>
           <Link to="/#contact" onClick={closeMenu}>Contact</Link>
